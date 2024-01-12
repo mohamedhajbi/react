605c33e66160894676fcfa8c7ab1864446d9049d
Mohamed Hajbi <hajbi662@gmail.com>
2024-01-12 15:24:35 +0000

    table

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,20 +1,37 @@
-import Todo from "./components/Todo"; 
+import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />); 
-  const btnList = props.filters.map((filter) => <FilterButton id={filter.id} name={filter.name} key={filter.id} />); 
+  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />);
+  const btnList = props.filters.map((filter) => <FilterButton id={filter.id} name={filter.name} key={filter.id} />);
+  const remainingTasks = props.tasks.filter((task) => !task.completed).length;
+  const completedTasks = props.tasks.filter((task) => task.completed).length;
+  const allTasks = props.tasks.length;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-
-        <Form />
-     
+      <Form />
       <div className="filters btn-group stack-exception">
         {btnList}
       </div>
-      <h2 id="list-heading">3 tâches restantes</h2>
+      <table>
+      <thead>
+          <tr>
+            <th>Tâches terminées</th>
+            <th>Tâches restantes</th>
+            <th>Total de tâches</th>
+          </tr>
+        </thead>
+        <tbody>
+      <tr>
+        <td>{completedTasks}</td>
+        <td>{remainingTasks}</td>
+        <td>{allTasks}</td>
+      </tr>
+      </tbody>
+      </table>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception">
