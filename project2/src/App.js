3a6dd5d335504279408454cf94dd5b3c7e5b43a6
Mohamed Hajbi <hajbi662@gmail.com>
2024-01-12 14:56:09 +0000

    components FilterButtom/From

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,50 +1,24 @@
 import Todo from "./components/Todo"; 
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
 function App(props) {
+  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />); 
+  const btnList = props.filters.map((filter) => <FilterButton id={filter.id} name={filter.name} key={filter.id} />); 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            Qu'y a-t-il à faire&nbsp;?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Ajouter
-        </button>
-      </form>
+
+        <Form />
+     
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" >
-          <span className="visually-hidden">Montrer </span>
-          <span>Toutes</span>
-          <span className="visually-hidden"> les tâches</span>
-        </button>
-        <button type="button" className="btn toggle-btn" >
-          <span className="visually-hidden">Montrer </span>
-          <span className="visually-hidden">les tâches </span>
-          <span>Actives</span>
-        </button>
-        <button type="button" className="btn toggle-btn" >
-          <span className="visually-hidden">Montrer </span>
-          <span className="visually-hidden">les tâches </span>
-          <span>Terminées</span>
-        </button>
+        {btnList}
       </div>
       <h2 id="list-heading">3 tâches restantes</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception">
-        <Todo name="Manger" completed={true} id="todo-0" />
-        <Todo name="Dormir" completed={false} id="todo-1" />
-        <Todo name="Recommencer" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   );
--- project2/src/index.js
+++ project2/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Manger", completed: true }, 
+  { id: "todo-1", name: "Dormir", completed: false },
+  { id: "todo-2", name: "Recommencer", completed: false },
+ ];
+
+const DATA2 = [ 
+  {id : "1" , name: "All"},
+  {id : "2" , name: "Active"},
+  {id : "3" , name: "Completed"},
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} filters={DATA2} />
   </React.StrictMode>
 );
 
